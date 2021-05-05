1fd8378d065cd638d06c57c56515ebb39a174bbc
Julius Lee <7julius.lee@gmail.com>
2021-05-05 06:21:16 -0700

    setup-morgan-logger

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ import bootcampRoute from './routes/bootcamps.route.js'
 dotenv.config({ path: './config/config.env' })
 const app = express()
 app.use(express.json())
-app.use(morgan('dev'))
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use('/api/v1/bootcamps', bootcampRoute)
 
