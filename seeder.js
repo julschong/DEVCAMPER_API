57db12ec4ace8a97278b5d26e77ac5b26b980c7a
Julius Lee <7julius.lee@gmail.com>
2021-05-13 20:52:48 -0700

    added average cost aggr

--- seeder.js
+++ seeder.js
@@ -25,7 +25,7 @@ const courses = JSON.parse(fs.readFileSync(`./_data/courses.json`, 'utf-8'));
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-        await Course.create(courses);
+        // await Course.create(courses);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (e) {
